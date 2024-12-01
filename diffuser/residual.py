d1bcb1815a9b615f4b257b656bdc48aeecf14b01
ccozad <ccozad@gmail.com>
2024-11-30 22:32:45 -0800

    Start a low level diffuser example

---
+++ diffuser/residual.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
