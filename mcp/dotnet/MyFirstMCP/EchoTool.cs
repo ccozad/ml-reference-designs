ef08a709d838a2a868791a080a4b6f138f5e9baf
ccozad <ccozad@gmail.com>
2025-04-23 22:36:23 -0700

    Echo Tool in MCP server

---
+++ mcp/dotnet/MyFirstMCP/EchoTool.cs
@@ -0,0 +1,12 @@
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+}
